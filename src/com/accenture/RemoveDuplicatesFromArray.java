bc838e96bddf7da9bcf30ac142bf76ca87958a99
Pankaj <pankajppatil1008@gmail.com>
2025-06-17 13:20:59 +0530

    code change

--- src/com/accenture/RemoveDuplicatesFromArray.java
+++ src/com/accenture/RemoveDuplicatesFromArray.java
@@ -6,7 +6,9 @@ public class RemoveDuplicatesFromArray {
     public static void main(String[] args) {
         int[] arr = {3,6,8,2,5,7,9,3,2,7,9,9,3,2,6};
         int j=0;
+
         Arrays.sort(arr);
+
         System.out.println(Arrays.toString(arr));
         for(int i=1; i<arr.length; i++) {
             if(arr[j]!=arr[i]){
@@ -14,6 +16,7 @@ public class RemoveDuplicatesFromArray {
                 arr[j]=arr[i];
             }
         }
+
         for(int i=0; i<j; i++){
             System.out.print(" " + arr[i] );
         }
