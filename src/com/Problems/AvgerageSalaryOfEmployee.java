88000302bf847828828c29b96bfd3ca83ab6bfe8
Pankaj <pankajppatil1008@gmail.com>
2025-07-04 10:41:30 +0530

    code change

--- src/com/Problems/AvgerageSalaryOfEmployee.java
+++ src/com/Problems/AvgerageSalaryOfEmployee.java
@@ -2,6 +2,7 @@ package com.Problems;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.OptionalDouble;
 
 
 //find the average salary of employee using stream
@@ -15,7 +16,10 @@ public class AvgerageSalaryOfEmployee {
 			Employee emp = new Employee("Pankaj" + i , 10_000+i*1000, 126);
 			list.add(emp);
 		}
-		
+
+		OptionalDouble d = list.stream().mapToDouble(e-> e.getSalary()).average();
+		System.out.println(d);
+
 		System.out.println("Average Salary " + list.stream().mapToDouble(Employee::getSalary).average());
 		
 		
